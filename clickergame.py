30cb762813969458883096493b3d82656800a263
Matthew <largecoatman@gmail.com>
2025-10-24 18:55:36 +0100

    New Type Upgrade

--- clickergame.py
+++ clickergame.py
@@ -11,25 +11,27 @@ pygame.display.set_caption("clicker")
 # database create file and table
 con = sqlite3.connect("stats.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS scores(points REAL, perclick REAL, cps REAL, totalclicks REAL, totaltime REAL, upgrade1 REAL, upgrade2 REAL)")
+cur.execute("CREATE TABLE IF NOT EXISTS scores(points REAL, perclick REAL, cps REAL, totalclicks REAL, totaltime REAL, upgrade1 REAL, upgrade2 REAL, bonus1 REAL)")
 
 # reads the data from database
-cur.execute("SELECT points, perclick, cps, totalclicks, totaltime, upgrade1, upgrade2 FROM scores ORDER BY rowid DESC LIMIT 1")
+cur.execute("SELECT points, perclick, cps, totalclicks, totaltime, upgrade1, upgrade2, bonus1 FROM scores ORDER BY rowid DESC LIMIT 1")
 row = cur.fetchone()
 
 #set the variables to the read data, if there is no data than set to default values
 if row:
-    points, increase_on_click, clicks_per_second, totalclicks, totaltime, isupgrade1, isupgrade2 = row
+    points, increase_on_click, clicks_per_second, totalclicks, totaltime, isupgrade1, isupgrade2, isbonus1 = row
     isupgrade1 = bool(isupgrade1)
     isupgrade2 = bool(isupgrade2)
+    isbonus1 = bool(isbonus1)
 else:
-    points = 0
+    points = 10000
     increase_on_click = 1
     clicks_per_second = 0
     totalclicks = 0
     totaltime = 0
     isupgrade1 = False
     isupgrade2 = False
+    isbonus1 = False
 
 
 #button images
@@ -41,7 +43,7 @@ click_per_sec1 = pygame.image.load("Finch_Staff.png").convert_alpha()
 click_per_sec2 = pygame.image.load("Flinx_Staff.png").convert_alpha()
 no = pygame.image.load("Red X.png").convert_alpha()
 statimg = pygame.image.load("statsbtn.png").convert_alpha()
-
+shackle = pygame.image.load("Shackle.png").convert_alpha()
 
 
 #fonts
@@ -55,13 +57,14 @@ text_surface = font2.render(str(points), True, (255, 255, 255))
 clkl = font.render("Click For Coins", True, (255, 255, 255))
 upgrades = font.render("Upgrades", True, (255, 255, 255))
 auto = font.render("CPS Upgrades", True, (255, 255, 255))
-cps = font2.render(f"cps: {str(clicks_per_second)}", True, (255, 255, 255))
-per_click = font2.render(f"Per Click: {increase_on_click}", True, (255, 255, 255))
+bonuses = font.render("Bonuses", True, (255, 255, 255))
 upgrade1_cost = font2.render("150|+1", True, (255, 255, 255))
 upgrade2_cost = font2.render("500|+2", True, (255, 255, 255))
 auto1 = font2.render("50|+0.25", True, (255, 255, 255))
 auto2 = font2.render("150|+1", True, (255, 255, 255))
-
+bonus1price = font2.render("1000|+10%", True, (255, 255, 255))
+cps = font2.render(f"cps: {str(clicks_per_second)}", True, (255, 255, 255))
+per_click = font2.render(f"Per Click: {increase_on_click}", True, (255, 255, 255))
 
 
 #renders the text surfaces on screen
@@ -76,6 +79,8 @@ screen.blit(upgrades, (425, 10))
 screen.blit(click_img, (0, 0))
 screen.blit(clkl, (10, 100))
 screen.blit(text_surface, (10, 10))
+screen.blit(bonuses, (220, 10))
+screen.blit(bonus1price, (220,107))
 
 #button class
 class BUTTON:
@@ -157,6 +162,20 @@ class BUTTON:
                     text_surface = font2.render(str(points), True, (255, 255, 255))
                     screen.blit(text_surface, (10, 10))
         screen.blit(self.image, (self.rect.x, self.rect.y))
+    def bonus(self, events):
+        global points, clicks_per_second, isbonus1
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button ==1 and points >= 1000 and isbonus1 == False:
+                    points -= 1000
+                    isbonus1 = True
+                    clicks_per_second = clicks_per_second * 1.10
+                    screen.fill(pygame.Color("black"), (10, 10, 100, 25))
+                    text_surface = font2.render(str(points), True, (255, 255, 255))
+                    screen.blit(text_surface, (10, 10))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
 
     def statistics(self, events):
         global totalclicks, totaltime, time1
@@ -187,6 +206,8 @@ upgrade2btn = BUTTON(434, 140, upgrade2, scale=1.85, held=True)
 auto1 = BUTTON(333, 45, click_per_sec1, scale=1.6, held=True)
 auto2 = BUTTON(333, 140, click_per_sec2, scale=1.6, held=True)
 statsbtn = BUTTON(10,368, statimg, scale=1.6, held=True)
+bonus1 = BUTTON(220,45, shackle, scale=2.6, held=True)
+
 screen.blit(upgrade1btn.image, upgrade1btn.rect)
 screen.blit(statsbtn.image, statsbtn.rect)
 last_update_time = pygame.time.get_ticks()
@@ -205,13 +226,14 @@ while run:
     for event in events:
         if event.type == pygame.QUIT:
             totaltime = time1 + totaltime
-            cur.execute("INSERT INTO scores (points, perclick, cps, totalclicks,totaltime, upgrade1, upgrade2) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                        (points, increase_on_click, clicks_per_second, totalclicks, totaltime, isupgrade1, isupgrade2))
+            cur.execute("INSERT INTO scores (points, perclick, cps, totalclicks,totaltime, upgrade1, upgrade2, bonus1) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                        (points, increase_on_click, clicks_per_second, totalclicks, totaltime, isupgrade1, isupgrade2, isbonus1))
             con.commit()
             run = False
 
     screen.fill(pygame.Color("black"), (10, 120, 150, 80))
     per_click = font2.render(f"Per Click: {increase_on_click}", True, (255, 255, 255))
+    clicks_per_second = round(clicks_per_second, 2)
     cps = font2.render(f"cps: {str(clicks_per_second)}", True, (255, 255, 255))
     screen.blit(cps, (10, 120))
     screen.blit(per_click, (10, 145))
@@ -223,6 +245,7 @@ while run:
     upgrade2btn.upgrade2(events)
     auto1.per_second1(events)
     auto2.per_second2(events)
+    bonus1.bonus(events)
     statsbtn.statistics(events)
 
 
@@ -230,13 +253,15 @@ while run:
         screen.blit(no, (434, 45))
     if isupgrade2:
         screen.blit(no, (434, 140))
-
+    if isbonus1:
+        screen.blit(no, (220, 45))
 
     current_time = pygame.time.get_ticks()
     if current_time - last_update_time >= 1000:
         points += clicks_per_second
         last_update_time = current_time
         screen.fill(pygame.Color("black"), (10, 10, 100, 25))
+        points = round(points, 2)
         text_surface = font2.render(str(points), True, (255, 255, 255))
         screen.blit(text_surface, (10, 10))
 
